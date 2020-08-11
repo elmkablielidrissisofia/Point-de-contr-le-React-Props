6efcf2d72f3250f4caf21abf2ce540f52802c59e
elmkablielidrissisofia <sofia.elidrissi1989@gmail.com>
2020-08-11 10:04:11 +0100

    first commit

---
+++ src/Profile/profile.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Profile = (props) => {
+    return ( <div>
+        <p>{props.FullName}</p>
+    </div> );
+}
+const Profile = (props) => {
+    return ( <div>
+        <p>{props.bio}</p>
+    </div> );
+}
+const Profile = (props) => {
+    return ( <div>
+        <p>{props.profession}</p>
+    </div> );
+}
+
+ 
+ 
+ 
+export default Profile;
